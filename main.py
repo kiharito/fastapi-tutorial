9362ed8d6ed4650546ba04e610152e62009f468e
kiharito <kihara13611.c7d7@gmail.com>
2024-01-06 18:12:45 +0900

    リクエストボディ

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from enum import Enum
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 
 app = FastAPI()
 
@@ -21,6 +30,15 @@ async def root():
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_item_db[skip: skip + limit]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
 @app.get("/items/{item_id}")
